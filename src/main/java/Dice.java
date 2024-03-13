f8832aa046acd7ece2b5661823d1fbcb0771817e
andyzheng679 <andyzheng679@gmail.com>
2024-03-12 21:56:51 -0400

    Dice class

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,24 @@
+import java.util.Random;
+
 public class Dice {
+
+    private int numOfDicesRolled;
+    private Random random; //creating an object of Random
+
+    //constructor
+    public Dice(int numOfDicesRolled){
+        this.numOfDicesRolled = numOfDicesRolled;
+        this.random = new Random();
+    }
+
+    public int tossAndSum(){
+        int sum = 0;
+
+        for(int i = 0; i < this.numOfDicesRolled; i++){
+            sum += random.nextInt(6) + 1;   //roll dice from 0-5, +1 bc its a 6 sided dice
+        }
+        return sum;
+    }
 
 
 }
