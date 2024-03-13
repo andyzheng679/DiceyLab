ebcb3b6688df97665537ba35ef40c2b5589d05e1
andyzheng679 <andyzheng679@gmail.com>
2024-03-13 14:31:39 -0400

    dice and bins

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,4 +1,23 @@
 
 public class Bins {
+    private int[] resultHolder;
+    private int lowestRoll;
+    private int highestRoll;
+
+    public Bins(int lowestRoll, int highestRoll){
+        this.lowestRoll = lowestRoll;
+        this.highestRoll = highestRoll;
+        resultHolder = new int[this.highestRoll - this.lowestRoll + 1]; //2-12=10 but int[10] is only 9 slots, need + 1
+    }
+
+    public void increment(int rollResult){
+        resultHolder[rollResult - lowestRoll] += 1;
+    }
+
+    public int getBin(int index){
+
+        return resultHolder[index];
+    }
+
 
 }
